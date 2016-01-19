dc1c4e774d7cbf486d7593e35abc9b7a37dff6e5
Max Zhuravlov <kpojier@gmail.com>
2016-01-19 22:58:22 +0300

    Create Application class

---
+++ src/main/java/ru/teligent/core/Application.java
@@ -0,0 +1,32 @@
+package ru.teligent.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ *
+ * @author Max Zhuravlov
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * App logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    /**
+     * Enter point
+     * @param args arguments params
+     */
+    public static void main(String args[]) {
+        new SpringApplicationBuilder()
+                .bannerMode(Banner.Mode.OFF)
+                .sources(Application.class)
+                .run(args);
+    }
+
+}
