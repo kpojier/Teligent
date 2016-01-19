2ebef57a53b4602bb1da67416679b1327ce20916
Max Zhuravlov <kpojier@gmail.com>
2016-01-19 23:50:52 +0300

    Setup test environment

---
+++ src/test/java/ru/teligent/AppTest.java
@@ -0,0 +1,45 @@
+package ru.teligent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import ru.teligent.core.Application;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * @author Max Zhuravlov
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebAppConfiguration
+public class AppTest {
+
+    @Autowired
+    protected WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void controllersTest() throws Exception {
+        // Check health
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status",is("UP")));
+    }
+}
