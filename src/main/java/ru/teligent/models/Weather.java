530939a316525df2a03297841b9060d32ffa2e9f
Max Zhuravlov <kpojier@gmail.com>
2016-01-20 22:09:41 +0300

    Create Weather class

--- src/main/java/ru/teligent/models/Weather.java
+++ src/main/java/ru/teligent/models/Weather.java
@@ -1,7 +1,32 @@
 package ru.teligent.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
- * @author kpojier
+ * Weather information
+ * @author Max Zhuravlov
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Weather {
+
+    @JsonProperty("name")
+    private String cityName;
+    private TemperatureInfo tempInfo;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public TemperatureInfo getTempInfo() {
+        return tempInfo;
+    }
+
+    public void setTempInfo(TemperatureInfo tempInfo) {
+        this.tempInfo = tempInfo;
+    }
 }
