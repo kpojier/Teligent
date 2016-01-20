b659c83923cc41fbe93e790670af26d081eba435
Max Zhuravlov <kpojier@gmail.com>
2016-01-21 00:06:19 +0300

    Create Weather forecast loader

--- src/main/java/ru/teligent/services/RestWeatherLoader.java
+++ src/main/java/ru/teligent/services/RestWeatherLoader.java
@@ -2,6 +2,7 @@ package ru.teligent.services;
 
 import org.springframework.web.client.RestTemplate;
 import ru.teligent.models.Weather;
+import ru.teligent.models.WeatherForecast;
 
 /**
  * Weather loader
@@ -9,9 +10,14 @@ import ru.teligent.models.Weather;
  */
 public class RestWeatherLoader extends RestTemplate {
 
-    private static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s,%s&mode=json&units=metric&appid=2de143494c0b295cca9337e1e96b00e0";
+    private static final String WEATHER_URL  = "http://api.openweathermap.org/data/2.5/weather?q=%s,%s&mode=json&units=metric&appid=2de143494c0b295cca9337e1e96b00e0";
+    private static final String FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?q=%s,%s&mode=json&appid=2de143494c0b295cca9337e1e96b00e0";
 
     public Weather loadCurrentWeather(String cityName, String countryCode) {
         return getForObject(String.format(WEATHER_URL, cityName, countryCode), Weather.class);
+    }
+
+    public WeatherForecast loadWeatherForecast(String cityName, String countryCode) {
+        return getForObject(String.format(FORECAST_URL, cityName, countryCode), WeatherForecast.class);
     }
 }
